67f9418f32354b9c62482c1f08fdd054335603f0
fnordisch1 <#test#123>
2014-10-10 10:16:13 +0200

    Init

--- src/com/fnord/testing/Hello.java
+++ src/com/fnord/testing/Hello.java
@@ -1,7 +1,11 @@
 package com.fnord.testing;
 
 public class Hello {
+	public Hello() {
+		System.out.println("Hello");
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		new Hello();
 	}
 }
